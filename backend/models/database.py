220434367ec7bc37b619ece99a58f2e92329e835
Alex Volkov <aleksandr.volkov1101@gmail.com>
2025-03-03 00:58:06 +0300

    тестовая база на postgress

--- backend/models/database.py
+++ backend/models/database.py
@@ -8,19 +8,18 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-# Создаем строку подключения к SQLite
-DATABASE_URL = os.getenv('DATABASE_URL')  # Значение по умолчанию
+# Создаем строку подключения к PostgreSQL
+DATABASE_URL = os.getenv('DATABASE_URL')  # Используем значение из .env
 
 logging.basicConfig(level=logging.DEBUG)
 logging.info("Таблицы успешно созданы.")
 
 try:
     logging.debug("Подключаемся к базе данных...")
-    engine = create_engine(DATABASE_URL,
-                           connect_args={"check_same_thread": False})
+    engine = create_engine(DATABASE_URL)  # Убираем connect_args для PostgreSQL
     with engine.connect() as connection:
         logging.debug("Успешное подключение!")
-        result = connection.execute(text("SELECT 1"))
+        result = connection.execute(text("SELECT 1"))  # Используем text()
         logging.debug(f"Результат: {result.fetchone()}")
 except Exception as e:
     logging.error(f"Произошла ошибка: {str(e)}")
